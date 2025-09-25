ed483db3682c3754a98a63b1e42fc3b59556357a
avofe <mazurenkoi70@gmail.com>
2025-09-25 15:04:33 +0300

    add links & scrolling

--- src/components/Home/CatalogService/CatalogProduct.tsx
+++ src/components/Home/CatalogService/CatalogProduct.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import s from "./index.module.css";
 
 const CatalogService = () => {
@@ -8,37 +9,45 @@ const CatalogService = () => {
       <div className={`container pb mb`}>
         <h2 className={s.title}>Каталог товаров</h2>
         <div className={s.grid}>
-          <div
-            className={`${boxCommon} ${s.boxImage1}`}
-            role="img"
-            aria-label="Компьютеры"
-          >
-            <p>Компьютеры</p>
-          </div>
+          <Link to={"/catalog/1/computers"}>
+            <div
+              className={`${boxCommon} ${s.boxImage1}`}
+              role="img"
+              aria-label="Компьютеры"
+            >
+              <p>Компьютеры</p>
+            </div>
+          </Link>
 
-          <div
-            className={`${boxCommon} ${s.boxImage2}`}
-            role="img"
-            aria-label="Ноутбуки"
-          >
-            <p>Ноутбуки</p>
-          </div>
+          <Link to={"/catalog/2/notebooks"}>
+            <div
+              className={`${boxCommon} ${s.boxImage2}`}
+              role="img"
+              aria-label="Ноутбуки"
+            >
+              <p>Ноутбуки</p>
+            </div>
+          </Link>
 
-          <div
-            className={`${boxCommon} ${s.boxImage3}`}
-            role="img"
-            aria-label="Принтеры"
-          >
-            <p>Принтеры</p>
-          </div>
+          <Link to={"/catalog/3/printers"}>
+            <div
+              className={`${boxCommon} ${s.boxImage3}`}
+              role="img"
+              aria-label="Принтеры"
+            >
+              <p>Принтеры</p>
+            </div>
+          </Link>
 
-          <div
-            className={`${boxCommon} ${s.boxImage4}`}
-            role="img"
-            aria-label="Запчасти"
-          >
-            <p>Запчасти</p>
-          </div>
+          <Link to={"/catalog"}>
+            <div
+              className={`${boxCommon} ${s.boxImage4}`}
+              role="img"
+              aria-label="Запчасти"
+            >
+              <p>Запчасти</p>
+            </div>
+          </Link>
 
           <div className={s.boxBig}>
             <p className={s.boxBigText}>
@@ -53,10 +62,11 @@ const CatalogService = () => {
               alt="Каталог товаров"
               className={s.boxBigImage}
             />
-
-            <div className={s.arrow}>
-              <div className={s.arrow_clip}></div>
-            </div>
+            <Link to={"/catalog"}>
+              <div className={s.arrow}>
+                <div className={s.arrow_clip}></div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import ScrollToTop from "@/app/hooks/ScrollToTop";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <div>
+        <ScrollToTop />
         <Outlet />
       </div>
       <Footer />
